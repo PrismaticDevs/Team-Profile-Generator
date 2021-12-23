24a08cd74804635fb4352ba6b05912cb5e3c99aa
Matthew Brignola <matthewbrignola@ymail.com>
2021-12-23 16:53:55 -0700

    template altered

---
+++ index.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+const { template } = require('./src/template.js');
+
+inquirer.prompt([{
+            type: "input",
+            name: "name",
+            message: "What is he developer's first name?"
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: `What is the developer's email?`
+        },
+        {
+            type: 'input',
+            name: 'type',
+            message: `What type of rngineer is the developer?`
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: `What is the developer's GitHub username?`
+        }
+    ])
+    .then(answers => {
+        const data = template(answers);
+        fs.writeFile('./src/index.html', data, (err) => {
+            if (err) {
+                console.error(err);
+            }
+        })
+    })
+    .catch(err => {
+        console.error(err);
+    })
